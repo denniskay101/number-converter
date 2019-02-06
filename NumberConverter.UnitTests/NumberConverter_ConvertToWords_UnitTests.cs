fed9ea1b64166a69e6fb8d1b5a6066ab42960e57
Dennis Kamau <dennis@mcmsolutions.co.uk>
2019-02-06 15:16:02 +0300

    Add unit tests project

---
+++ NumberConverter.UnitTests/NumberConverter_ConvertToWords_UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NumberConverter.UnitTests
+{
+    [TestFixture]
+    public class NumberConverter_UnitTests
+    {
+        [Test]
+        public void Test_That_The_Tester_Is_Testing()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ NumberConverter.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumberConverter.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberConverter.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a76a7c11-2bf2-4b3c-b39f-1b245d0ff260")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
